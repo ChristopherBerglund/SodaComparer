a7beea23c232744d0a5c5511799da700b874245b
christopher.berglund <christopher.berglund@se360.se>
2025-02-19 16:17:30 +0100

    jao

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/SodaComparer/',
-  plugins: [
-    react(),
-    {
-      name: 'configure-mime-types',
-      configureServer(server) {
-        server.middlewares.use((req, res, next) => {
-          if (req.url.endsWith('.jsx')) {
-            res.setHeader('Content-Type', 'application/javascript');
-          }
-          next();
-        });
-      }
-    }
-  ],
+  plugins: [react()],
 })
