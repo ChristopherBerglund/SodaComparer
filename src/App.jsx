6b53f6b3f4d4120dec57714c860dea48e53a5647
christopher.berglund <christopher.berglund@se360.se>
2025-02-19 17:49:51 +0100

    johoråå

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-// import React from 'react';
+import React from 'react';
 import './App.css';
 import ClipLoader from "react-spinners/ClipLoader";
 import PepsiBoxFetcher from "./compontents/Willys/PepsiBoxFetcher.jsx";
