dd93218b8917255d4794a5d83041307afd05c77c
christopher.berglund <christopher.berglund@se360.se>
2025-02-19 17:28:07 +0100

    vite fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
-    outDir: 'dist', // Ensure this matches the workflow
+    base: 'SodaComparer', // Ensure this matches the workflow
   }
 });
