ad4f5107cc26b7420d7cf40e57e7db06b52f9d9f
christopher.berglund <christopher.berglund@se360.se>
2025-02-19 16:32:22 +0100

    daw

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/SodaComparer/',
-  plugins: [react()],
+  base: '/SodaComparer/', // Change to your repo name
 })
