5819e1109a1060588a124796bd91ec0efa46bb64
christopher.berglund <christopher.berglund@se360.se>
2025-02-19 17:41:40 +0100

    react fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import React from 'react';
 import './App.css';
 import ClipLoader from "react-spinners/ClipLoader";
 import PepsiBoxFetcher from "./compontents/Willys/PepsiBoxFetcher.jsx";
