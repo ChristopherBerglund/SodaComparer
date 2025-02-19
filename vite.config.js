ae251ac8aeed37f8c24f3498a9bb9591407f8453
christopher.berglund <christopher.berglund@se360.se>
2025-02-19 17:33:46 +0100

    sefsetsrfr

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
-    base: '/SodaComparer/', // Use a leading slash and match the GitHub Pages URL structure
+    base: '/SodaComparer/', 
   }
 });
